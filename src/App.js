91e8334f289f17d37a54879291fa13c953ebe16e
Hrushikesh <hrushipatil2019262@gmail.com>
2023-06-27 13:27:20 -0700

    props in class and functional components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import ClassBasedCom from "./Components/ClassBasedCom";
 import "./App.css"
+import FunctionBasedCom from "./Components/FunctionBasedCom";
+
 function App() {
   
   return (
     <>
       <div className="App">
-        <ClassBasedCom/>
+        <ClassBasedCom name="Hrushi"/>
+        <p>
+
+        </p>
+        <FunctionBasedCom count="2"/>
       </div>
     </>
   );
 }
 
 export default App;
+
